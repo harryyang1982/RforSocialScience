ff31ad1163dd8a4157c148b79c19867ebaab5b6a
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-30 23:05:41 +0900

    exercises

---
+++ codes/exercises1_cars.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(readxl)
+
+korean_cars <- read_csv("datasets/한국자동차_연비.csv",
+                        locale=locale(encoding="CP949")) %>% 
+  mutate(복합연비=as.numeric(substr(복합연비, 1, 4))) %>% 
+  filter(!is.na(복합연비))
+
+library(writexl)
+write_xlsx(korean_cars, "datasets/korean_cars.xlsx")
