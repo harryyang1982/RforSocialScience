df0a8341bc24270be15fbb2eb649fc011fab04df
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-30 23:11:40 +0900

    date and code updated

--- codes/exercises1_cars.R
+++ codes/exercises1_cars.R
@@ -4,7 +4,8 @@ library(readxl)
 korean_cars <- read_csv("datasets/한국자동차_연비.csv",
                         locale=locale(encoding="CP949")) %>% 
   mutate(복합연비=as.numeric(substr(복합연비, 1, 4))) %>% 
-  filter(!is.na(복합연비))
+  filter(!is.na(복합연비)) %>% 
+  rename(CO2배출량=`배출량(CO2)`)
 
 library(writexl)
 write_xlsx(korean_cars, "datasets/korean_cars.xlsx")
